f523ba03b82ad55813b127aa5ffb21974b574387
rickytheoldtree <rickyliyiqi@outlook.com>
2025-04-15 10:41:37 +0800

    v1.0.3

---
+++ Runtime/UIComponents/BindablePropertyButton.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace RicKit.RFramework.UIComponents
+{
+    [RequireComponent(typeof(Button))]
+    public abstract class BindablePropertyButton<T> : MonoBehaviour
+    {
+        protected BindableProperty<T> bp;
+        protected T id;
+        protected bool lastSelected;
+        protected virtual void Awake()
+        {
+            var btn = GetComponentInChildren<Button>();
+            btn.onClick.AddListener(() =>
+            {
+                if (!CanSelected()) return;
+                bp.Value = id;
+            });
+        }
+
+        protected virtual void OnDestroy()
+        {
+            bp?.UnRegister(OnValueChange);
+        }
+
+        public void Init(T id, BindableProperty<T> bp)
+        {
+            this.bp?.UnRegister(OnValueChange);
+            this.id = id;
+            this.bp = bp;
+            bp.Register(OnValueChange);
+            var selected = id.Equals(bp.Value);
+            InitUI(selected);
+            lastSelected = selected;
+        }
+
+        private void OnValueChange(T id)
+        {
+            var selected = id.Equals(this.id);
+            UpdateUI(selected, lastSelected);
+            lastSelected = selected;
+        }
+        protected abstract void InitUI(bool selected);
+        protected abstract void UpdateUI(bool selected, bool lastSelected);
+        protected virtual bool CanSelected() => true;
+    }
+}
