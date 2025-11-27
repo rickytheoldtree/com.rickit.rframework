9e8db258ecb3cd183c4c43c20f9c102e96792a2e
rickytheoldtree <rickyliyiqi@outlook.com>
2025-11-27 11:56:00 +0800

    v1.4.7

---
+++ CHANGELOG.md
@@ -0,0 +1,34 @@
+# Changelog
+## [1.4.7] - 2025-11-27
+- Introduces BindableCollection, BindableList, and BindableDictionary classes to support data binding for collections, lists, and dictionaries. Refactors and extends extension methods for collection operations to work with the new bindable types, enabling notification on collection changes.
+## [1.4.6] - 2025-11-26
+- Add BindableProperty Extension for ICollection<T>, IList<T>, HashSet<T>
+## [1.4.5] - 2025-09-29
+- Add ForceNotify and UnRegisterAll to BindableProperty
+## [1.4.4] - 2025-08-28
+- Refactor UpdateUI method in BindablePropertyButton
+## [1.4.3] - 2025-06-13
+- Rename SendCommandOnlyArgs method to SendCommand for consistency
+## [1.4.2] - 2025-06-12
+- refactor ICommand interfaces for improved execution handling
+## [1.3.4] - 2025-06-03
+- refactor: improve service initialization logic in ServiceLocator and AsyncSupport
+## [1.3.1] - 2025-05-27
+- refactor event handling in ServiceLocator to use Dictionary<Type, Delegate>
+## [1.3.0] - 2025-05-27
+- enhance ICommand interface to support variable arguments and update ServiceLocator to manage command instances
+## [1.2.2] - 2025-05-27
+- refactor event handling to use delegates for improved performance and flexibility
+## [1.2.1] - 2025-04-18
+- add: `Cache`
+## [1.1.1] - 2025-04-16
+- remove: `EnhancedScroller` from the package
+## [1.1.0] - 2025-04-16
+- add: `EnhancedScroller`
+## [1.0.3] - 2025-04-15
+- add: `lastSelected` field to `BindablePropertyButton`
+## [1.0.1] - 2025-03-19
+### Add BindablePropertyButton
+## [1.0.0] - 2025-03-07
+### This is the first release of *com.rickit.rframework*.
+- first commit
---
+++ Runtime/AsyncSupport.cs
@@ -0,0 +1,61 @@
+﻿#if USING_UNITASK
+using System;
+using Cysharp.Threading.Tasks;
+
+namespace RicKit.RFramework
+{
+    public interface ICanInitAsync
+    {
+        UniTask InitAsync(IProgress<float> progress);
+    }
+
+    public interface ICanStartAsync
+    {
+        UniTask StartAsync(IProgress<float> progress);
+    }
+
+    public abstract class AsyncServiceLocator<T> : ServiceLocator<T> where T : AsyncServiceLocator<T>, ICanInitAsync, new()
+    {
+        public static async UniTask InitializeAsync(IProgress<float> progress)
+        {
+            if (locator != null) return;
+            locator = new T();
+            await locator.InitAsync(progress);
+            foreach (var service in locator.cache)
+            {
+                if (service is ICanInitAsync initAsync)
+                    await initAsync.InitAsync(progress);
+                else
+                    service.Init();
+            }
+            foreach (var service in locator.cache)
+            {
+                if (service is ICanStartAsync startAsync)
+                    await startAsync.StartAsync(progress);
+                else
+                    service.Start();
+                service.IsInitialized = true;
+            }
+            locator.IsInitialized = true;
+        }
+        
+        public async UniTask RegisterServiceAsync<TService>(TService service, IProgress<float> progress) where TService : IService
+        {
+            service.SetLocator(this);
+            var type = typeof(TService);
+            if (!cache.TryAdd(type, service))
+                throw new ServiceAlreadyExistsException(type);
+            if (!IsInitialized) return;
+            if (service is ICanInitAsync initAsync)
+                await initAsync.InitAsync(progress);
+            else
+                service.Init();
+            if (service is ICanStartAsync startAsync)
+                await startAsync.StartAsync(progress);
+            else
+                service.Start();
+            service.IsInitialized = true;
+        }
+    }
+}
+#endif
