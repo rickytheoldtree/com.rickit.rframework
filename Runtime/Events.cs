bcec9177a6167be21dedacf0d5bfd6a80db6b5f6
rickytheoldtree <94957102+rickytheoldtree@users.noreply.github.com>
2026-06-01 11:54:56 +0800

    v1.5.1

---
+++ Runtime/Events.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RicKit.RFramework
+{
+    public static class EventExtension
+    {
+        public static void RegisterEvent<T>(this ICanGetLocator self, Action<T> action) =>
+            self.GetLocator().RegisterEvent(action);
+
+        public static void UnRegisterEvent<T>(this ICanGetLocator self, Action<T> action) =>
+            self.GetLocator().UnRegisterEvent(action);
+
+        public static void SendEvent<T>(this ICanGetLocator self, T arg = default) =>
+            self.GetLocator().SendEvent(arg);
+
+        public static void RegisterEvent<T>(this IServiceLocator self, Action<T> action)
+        {
+            var type = typeof(T);
+            if (self.Events.TryGetValue(type, out var existing))
+                self.Events[type] = Delegate.Combine(existing, action);
+            else
+                self.Events[type] = action;
+        }
+
+        public static void UnRegisterEvent<T>(this IServiceLocator self, Action<T> action)
+        {
+            var type = typeof(T);
+            if (!self.Events.TryGetValue(type, out var existing)) return;
+
+            var updated = Delegate.Remove(existing, action);
+            if (updated == null)
+                self.Events.Remove(type);
+            else
+                self.Events[type] = updated;
+        }
+
+        public static void SendEvent<T>(this IServiceLocator self, T arg = default)
+        {
+            var type = typeof(T);
+            if (self.Events.TryGetValue(type, out var d))
+                (d as Action<T>)?.Invoke(arg);
+        }
+    }
+}
---
+++ Runtime/Exceptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RicKit.RFramework
+{
+    public class ServiceNotFoundException : Exception
+    {
+        public ServiceNotFoundException(Type t) : base($"Service {t.Name} not found")
+        {
+        }
+    }
+
+    public class ServiceAlreadyExistsException : Exception
+    {
+        public ServiceAlreadyExistsException(Type t) : base($"Service {t.Name} already exists")
+        {
+        }
+    }
+}
